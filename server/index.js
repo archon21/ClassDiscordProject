16c2caa859f1b85b2352d938c71eacd3de93f225
archon21 <rwetmore3@gmail.com>
2020-04-27 20:02:55 -0400

    Basic download request

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const path = require('path');
+const ytdl = require('ytdl-core');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log(req.query);
-
   res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
+app.post('/api/download', async (req, res) => {
+  const { url } = req.body;
+  console.log(req.body);
+  const data = await ytdl.getInfo(url);
+  const { thumbnail_url } = data;
+  console.log(data);
+  res.status(200).json({ thumbnail_url });
+  //   ytdl.pipe(path.join(__dirname, '..', 'public'));
 });
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
